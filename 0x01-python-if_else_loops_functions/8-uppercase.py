85768dfceb52f79734d29aaacca32786358469d2
faatihat <tomfaah@gmail.com>
2022-11-30 12:46:37 -0800

    done8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    stp1 = ""
+    for letter in str:
+        if ord(letter) >= 97 and ord(letter) <= 122:
+            stp1 = stp1 + chr((ord(letter) - 32))
+        else:
+            stp1 = stp1 + letter
+    print(stp1)
