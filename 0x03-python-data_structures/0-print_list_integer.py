b4fb6ed182896a90aae88d418d82f79cae8604cd
faatihat <tomfaah@gmail.com>
2022-12-05 05:41:11 -0800

    done0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 
 def print_list_integers(my_list=[]):
+    """
+    prints integer in a list
+    Args:
+        my_list - list of integers default []
+        """
+
     for i in my_list:
         print("{:d}".format(i))
