187093c82596c8b9685da0a4225d83e67650d4ad
faatihat <tomfaah@gmail.com>
2022-12-05 05:54:51 -0800

    done1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
+    """
+    gets an elment from a list at index idx
+    Args:
+        my_list - list to search
+        idx - the position to access
+    Return:
+        None - if idx is out of range
+        Data - element at idx
+    """
+
     if idx < 0:
         return None
      elif idx > len(my_list):
