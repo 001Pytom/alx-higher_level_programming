30dba78c56a4bddceea1dc0bea3f8c064c10612b
faatihat <tomfaah@gmail.com>
2022-12-05 07:41:11 -0800

    done7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python3
 
-def add_tuple(a=(), b=()):
-    """
-    add the fisrst and second elems of a b
-    if any of a or b has less than 2 elems
-    0 is padded
-    Args:
-        a - tuple default empty
-        b - tuple default empty
-    Return:
-        (c, d)
-    """
-     while len(a) < 2:
-         a = (*a, 0)
-     while len(b) < 2:
-         b = (*b, 0)
-     return a[0] + b[0], a[1] + b[1]
+def add_tuple(tuple_a=(), tuple_b=()):
+    while len(tuple_a) < 2:
+        tuple_a = (*tuple_a, 0)
+    while len(tuple_b) < 2:
+        tuple_b = (*tuple_b, 0)
+    else:
+        return tuple_a[0]+tuple_b[0], tuple_a[1]+tuple_b[1]
